f97da72f267edef87cf271734c0c4ccaf8486fed
Jordi Cabot <jcabotsagrera@gmail.com>
2019-05-08 07:04:09 +0200

    Adding core public view files

---
+++ public/partials/serious-toxic-comments-public-display.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ *
+         * @package    Serious_Toxic_Comments
+ * @subpackage Serious_Toxic_Comments\public\partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
