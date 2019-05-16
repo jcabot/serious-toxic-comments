3e4d514bf336b68c742fc441ef33292c4e134482
Jordi Cabot <jcabotsagrera@gmail.com>
2019-05-16 07:13:14 +0200

    Adding the license and readme.txt files

---
+++ README.txt
@@ -0,0 +1,46 @@
+=== Serious Toxic Comments ===
+Contributors: softmodeling, seriouswp
+Tags: comments, toxic, toxicity, AI, tensorflow, insults, offensive, threats, machine learning
+Requires at least: 4.3
+Tested up to: 5.2
+Requires PHP: 5.6
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Flag and block toxic comments from polluting your site with insults, threats, obscenities, etc.
+
+== Description ==
+
+Toxic comments are becoming a major challenge to have meaningful online discussions.
+
+This plugin uses a pre-trained toxic classifier from [TensorFlow] (https://www.tensorflow.org/) to classify a comment as toxic. See more technical details on the quality of the model [here] (https://medium.com/tensorflow/text-classification-using-tensorflow-js-an-example-of-detecting-offensive-language-in-browser-e2b94e3565ce).
+
+Once a comment is flagged as toxic, the comment is blocked and the plugin alerts the comment author and asks to modify the text before trying again.
+
+In the default *Settings->Discussion* page you can enable the detection of toxic comments and define the threshold confidence level for the predicition.
+
+== Installation ==
+
+Install and Activate the plugin through the 'Plugins' menu in WordPress
+
+
+== Frequently Asked Questions ==
+
+= How did you train the predictive model? =
+
+We didn't. We are using a pre-trained model provided by tensorflow itself.
+
+= Can I improve or personalize the prediction by manually training the toxicity classifier on my site? =
+
+No. The classifier is pre-trained. But you could build your own classifier based on the [code to create and train] (https://github.com/conversationai/conversationai-models/tree/master/experiments) this one
+
+== Screenshots ==
+
+1. Configuration settings for the plugin
+2. Example of a blocked comment
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
--- public/class-serious-toxic-comments-public-ext.php
+++ public/class-serious-toxic-comments-public-ext.php
@@ -16,55 +16,54 @@ class Serious_Toxic_Comments_Public_Ext extends Serious_Toxic_Comments_Public {
 		wp_enqueue_script( 'tf', 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs', array( 'jquery' ), null, false );
 		wp_enqueue_script( 'tfencode', 'https://cdn.jsdelivr.net/npm/@tensorflow-models/universal-sentence-encoder', array( 'jquery' ), null, false );
 		wp_enqueue_script( 'tftox', 'https://cdn.jsdelivr.net/npm/@tensorflow-models/toxicity', array( 'jquery' ), null, false );
+	    //Scripts to enqueue depart from the original instructions (https://medium.com/tensorflow/text-classification-using-tensorflow-js-an-example-of-detecting-offensive-language-in-browser-e2b94e3565ce) and follow https://github.com/tensorflow/tfjs-models/commit/1c79559dc96fe1096310b5b35dc9ede1c681be87
+
 	}
 
 
 	public function comment_toxicity()
 	{
-		$options = get_option( 'discussion' );
+		$options = get_option( 'settingsToxic' );
+		$threshold = $options['threshold']/100;
 	    if(is_single() && comments_open() && isset($options['toxicdetection']) ) {
 
 			?>
-
 			<script>
-                //var ctx = document.getElementById("myChart").getContext('2d');
-                alert("Hola ! I am an alert box!!");
+               window.onload=function() {
+                   var commentForm = document.getElementById('commentform');
+                   commentForm.addEventListener('submit', function(event){
 
-                window.onload=function() {
-                    document.getElementById('commentform').onsubmit=function() {
-                        /* do what you want with the form */
-                        const textComment = document.getElementById('comment').value;
+                      async function classify(input) {
+                           // Load the model. Users optionally pass in a threshold and an array of labels to include.
+                          let toxic=false;
+                          const model = await toxicity.load(<?php echo $threshold; ?>);
+                           const results = await model.classify(input);
 
-                        // Should be triggered on form submit
-                      //  alert( textComment.concat(' submitted') );
-                        // You must return false to prevent the default form behavior
+                           for (const r of results) {
+                            //   alert(r.label.concat(r.results[0].match.toString()));
+                               if (r.results[0].match) {
+                                   toxic = true;
+                                   break;
+                               }
+                           }
+                           return toxic;//  alert(results[0].label.concat(results[0].results[0].match.toString()));
+                      }
 
-                        const threshold = 0.9;// The minimum prediction confidence.
-                        // Load the model. Users optionally pass in a threshold and an array of labels to include.
+                       event.preventDefault();
+                       const textComment = document.getElementById('comment').value;
+                       classify([textComment]).then(result => {
+                           if (result) {
+                              alert('Your comment has been flagged as toxic and cannot be submitted at this point. This can happen due to a variety of reasons (insults, obscenity, racism,...). Please, edit your comment and try again');
+                           }
+                           else  {
+                               //console.log(commentForm);
+                               HTMLFormElement.prototype.submit.call(commentForm)
+                               //commentForm.submit(); <- This doesn't work due to https://stackoverflow.com/questions/56106508/submit-comment-form-inside-an-async-await-then-javascript-block-in-wordpress
+                           }
+                       })
+                    })
 
-
-                        let model, labels;
-
-                        const classify = async (input) => {
-                            model = await toxicity.load(<?php echo $options['threshold']; ?>);
-                            const results = await model.classify(input);
-                            alert(results[0].label.concat(results[0].results[0].match.toString()));
-                            alert(results[1].label.concat(results[1].results[0].match.toString()));
-                        }
-
-                           // results.forEach((classification) => {
-                            //        obj[classification.label] = classification.results[i].match;
-                             //   });
-                              //  return obj;
-                            //});
-                        //};
-
-                        classify([textComment]);
-
-                        return false;
-                    }
-                }
-
+               }
 			</script>
 			<?php
 		}
